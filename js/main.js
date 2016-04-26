7aeaad7cade9b12e82cc58b451fc8c15b09fafc9
Evelyn Hung <jj.evelyn@gmail.com>
2016-04-26 14:21:42 +0800

    UI polish
    - add file info
    - hide verbose option

--- js/main.js
+++ js/main.js
@@ -11,13 +11,16 @@ var PPParser = function() {
 PPParser.prototype.init = function() {
   this.container = document.getElementById('log_' + this.id);
   this.logFile = this.container.querySelector('.logFile');
-  this.logFile.addEventListener('change', function(evt) {
+  this.logFile.addEventListener('change', (function(evt) {
     var file = evt.target.files[0];
     // TODO file size check
     console.log(file.size);
-  });
+    this.fileInfoArea.textContent = "file size: " + file.size + ' Bytes';
+  }).bind(this));
+  this.fileInfoArea = this.container.querySelector('.fileInfo');
   this.submitBtn = this.container.querySelector('.submitBtn');
   this.submitBtn.addEventListener('click', this.readLog.bind(this));
+  // TODO listen to verbose state and show more logs
   this.verboseCheck = this.container.querySelector('.verbose');
   this.interfaceArea = this.container.querySelector('.interfacePicker');
   this.resultArea = this.container.querySelector('.result');
@@ -56,7 +59,7 @@ PPParser.prototype.handleLog = function(text) {
         }
         msg = line;
       }
-    } else {  // PPAPI log
+    } else {  // API log
       if (line.startsWith('callFromJSON')) {
         style = (line.indexOf(': <'))? 'fromPlugin' : 'fromBrowser';
       } else if (line.indexOf('From plugin') > 0) {
